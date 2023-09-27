65682e7ee4d6c532afcb1a3ad450fb86477a9ff0
Firdaus Dharma <firdausdharma@gmail.com>
2023-09-27 21:53:08 +0700

    update feature

--- streamlit_ngram.py
+++ streamlit_ngram.py
@@ -128,27 +128,23 @@ def main():
 
     st.divider()
 
-    if user_input_ngram:
-        m = create_ngram_model({user_input_ngram}, 'output.txt')
+    m = create_ngram_model({user_input_ngram}, 'output.txt')
 
-        user_input_sentence = st.text_input("Enter the initial sentence :",key=int,step=1)
-        
-        st.divider()
+    user_input_sentence = st.text_input("Enter the initial sentence :",key=int,step=1)
+    
+    st.divider()
 
-        user_input_len_text = st.number_input("Enter how many words are generated",key=int,step=1)
+    user_input_len_text = st.number_input("Enter how many words are generated",key=int,step=1)
 
-        st.divider()
+    st.divider()
+    
+    generated_text = m.generate_text({user_input_len_text})  # Generate 20 tokens based on the user input
 
-        if user_input_sentence and user_input_len_text:
-            
+    user_input_with_underscore = f'<u>{user_input_sentence}</u>'
 
-            generated_text = m.generate_text({user_input_len_text})  # Generate 20 tokens based on the user input
+    st.write(f'Generated text: {user_input_with_underscore} {generated_text}')
 
-            user_input_with_underscore = f'<u>{user_input_sentence}</u>'
-            
-            st.write(f'Generated text: {user_input_with_underscore} {generated_text}')
-
-            st.divider()
+    st.divider()
 
 if __name__ == "__main__":
     main()
